beabd8c2de013c9d83c959de91559d9ede7818b7
mark0g111 <9154547225@mail.ru>
2023-02-04 11:26:44 +0300

    hanoi

---
+++ 05_hanoi.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self._container = []
+
+    def push(self, item):
+        self._container.append(item)
+
+    def pop(self):
+        return self._container.pop()
+
+    def __repr__(self):
+        return repr(self._container)
+
+
+num_discs = 3
+tower_a = Stack()
+tower_b = Stack()
+tower_c = Stack()
+for i in range(1, num_discs + 1):
+    tower_a.push(i)
+
+
+def hanoi(begin, end, temp, n):
+    if n == 1:
+        end.push(begin.pop())
+    else:
+        hanoi(begin, temp, end, n-1)
+        hanoi(begin, end, temp, 1)
+        hanoi(temp, end, begin, n-1)
+
+
+if __name__ == '__main__':
+    hanoi(tower_a, tower_b, tower_c, num_discs)
+    print(tower_a)
+    print(tower_b)
+    print(tower_c)
